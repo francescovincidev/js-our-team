f17990086267171898cc997a3183ca85eda1a4d8
francescovincidev <francescovinci2000@gmail.com>
2023-03-22 13:14:52 +0100

    MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let teamArray =[
+    {
+        nome: "Wayne Barnett",
+        lavoro: "Founder & CEO",
+        foto: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela Caroll",
+        lavoro: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter Gordon",
+        lavoro: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela Lopez",
+        lavoro: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Scott Estrada",
+        lavoro: "Developer",
+        foto: "scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Barbara Ramos",
+        lavoro: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg"
+    }
+];
